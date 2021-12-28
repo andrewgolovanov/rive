12c4311eceb7d3ec7acefb8d20b01fd59b50aaad
Andrew Golovanov <spacewebdeveloper@gmail.com>
2021-12-29 01:30:53 +0300

    feat(components): add the AnimationWater

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import AnimationGuitar from './components/animation-guitar';
+import AnimationWater from './components/animation-water';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+  >
+    <AnimationGuitar />
+    <AnimationWater />
+  </div>
+);
 
 export default App;
---
+++ src/components/animation-water/animation-water.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { useRive, useStateMachineInput } from 'rive-react';
+
+import animationData from './data/animation.riv';
+
+const STATE_MACHINE_NAME = 'State Machine';
+const INPUT_NAME = 'Level';
+
+const AnimationWater = () => {
+  const params = {
+    src: animationData,
+    autoplay: true,
+    stateMachines: STATE_MACHINE_NAME,
+  };
+
+  const { RiveComponent, rive } = useRive(params, {
+    // If true, then the canvas will resize based on the height of the artboard. Defaults to false
+    fitCanvasToArtboardHeight: true,
+  });
+
+  /*
+   * @param {object} rive.
+   * @param {string} stateMachineName: name Name of the state machine.
+   * @param {string} inputName: Name of the state machine input
+   */
+  const stateRange = useStateMachineInput(rive, STATE_MACHINE_NAME, INPUT_NAME);
+
+  useEffect(() => {
+    if (!rive) return;
+
+    // @return {object} stateMachineName, inputName, ...
+    console.log('Water:', rive.contents);
+  }, [rive]);
+
+  const handleChange = ({ target: { value } }) => (stateRange.value = value);
+
+  return (
+    <div
+      style={{ marginTop: '100px', marginBottom: '100px', textAlign: 'center' }}
+    >
+      <RiveComponent style={{ width: 1000 }} />
+      <input type='range' onChange={handleChange} defaultValue={0} />
+    </div>
+  );
+};
+
+export default AnimationWater;
